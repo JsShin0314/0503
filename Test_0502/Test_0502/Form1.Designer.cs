9e6061f1e43d7fb8045b27dd245dcbfa546106a3
JsShin0314 <nicesun314@gmail.com>
2019-05-03 11:53:00 +0900

    0503

---
+++ Test_0502/Test_0502/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Test_0502
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LoadBtn_Img = new System.Windows.Forms.Button();
+            this.BtnLeft = new System.Windows.Forms.Button();
+            this.BtnRight = new System.Windows.Forms.Button();
+            this.BtnCount = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 129);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 155);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // LoadBtn_Img
+            // 
+            this.LoadBtn_Img.Location = new System.Drawing.Point(18, 173);
+            this.LoadBtn_Img.Name = "LoadBtn_Img";
+            this.LoadBtn_Img.Size = new System.Drawing.Size(75, 23);
+            this.LoadBtn_Img.TabIndex = 2;
+            this.LoadBtn_Img.Text = "Load";
+            this.LoadBtn_Img.UseVisualStyleBackColor = true;
+            this.LoadBtn_Img.Click += new System.EventHandler(this.LoadBtn_Img_Click);
+            // 
+            // BtnLeft
+            // 
+            this.BtnLeft.Location = new System.Drawing.Point(138, 173);
+            this.BtnLeft.Name = "BtnLeft";
+            this.BtnLeft.Size = new System.Drawing.Size(20, 23);
+            this.BtnLeft.TabIndex = 3;
+            this.BtnLeft.Text = "◀";
+            this.BtnLeft.UseVisualStyleBackColor = true;
+            this.BtnLeft.Click += new System.EventHandler(this.BtnControl_Click);
+            // 
+            // BtnRight
+            // 
+            this.BtnRight.Location = new System.Drawing.Point(245, 173);
+            this.BtnRight.Name = "BtnRight";
+            this.BtnRight.Size = new System.Drawing.Size(21, 23);
+            this.BtnRight.TabIndex = 5;
+            this.BtnRight.Text = "▶";
+            this.BtnRight.UseVisualStyleBackColor = true;
+            this.BtnRight.Click += new System.EventHandler(this.BtnControl_Click);
+            // 
+            // BtnCount
+            // 
+            this.BtnCount.Enabled = false;
+            this.BtnCount.Location = new System.Drawing.Point(164, 173);
+            this.BtnCount.Name = "BtnCount";
+            this.BtnCount.Size = new System.Drawing.Size(75, 23);
+            this.BtnCount.TabIndex = 4;
+            this.BtnCount.Text = "Count";
+            this.BtnCount.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.BtnRight);
+            this.Controls.Add(this.BtnCount);
+            this.Controls.Add(this.BtnLeft);
+            this.Controls.Add(this.LoadBtn_Img);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button LoadBtn_Img;
+        private System.Windows.Forms.Button BtnLeft;
+        private System.Windows.Forms.Button BtnRight;
+        private System.Windows.Forms.Button BtnCount;
+    }
+}
+
---
+++ Test_0502/Test_0502/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_0502
+{
+    public partial class Form1 : Form
+    {
+        string LoadFileName = string.Empty;
+        List<string> ListLoadFileName = new List<string>();
+        int CurrentImgNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void OpenFileDialog()
+        {
+            List<string> rList = new List<string>();
+
+            using (OpenFileDialog opd = new OpenFileDialog())
+            {
+                // 기본 파일 타입 설정
+                opd.DefaultExt = "All files";
+                opd.Filter = "All files (*.*)|*.*";
+                
+                //다중선택
+                //opd.Multiselect = false;
+                opd.Multiselect = true;
+                
+                string strAppDir = Environment.GetFolderPath((Environment.SpecialFolder.MyDocuments));
+
+                // 파일 불러오기를 했을 때 제일 처음에 열리는 디렉토리 설정
+                opd.InitialDirectory = strAppDir;
+
+                if(opd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        //if(opd.SafeFileName.LastIndexOf(".") > -1)
+                        //{
+                        //    MessageBox.Show("확장자가 존재하는 파일은 선택하실 수 없습니다.");
+
+                        //    return;
+                        //}
+
+                        //단일 선택
+                        //string fileName = opd.FileName;                        
+                        //LoadFileName = fileName;
+
+                        //다중 선택
+                        if (ListLoadFileName.Count > 0)
+                            ListLoadFileName = new List<string>();
+
+                        for(int i = 0; i < opd.FileNames.Length; i++)
+                        {
+                            ListLoadFileName.Add(opd.FileNames[i]);
+                        }
+
+                        BtnCount.Text = string.Format("({0}/{1})", CurrentImgNum+1, opd.FileNames.Length);
+
+                        //선택한 파일을 Open
+                        //rList = ReadTextFileToList(fileName);
+                    }
+                    catch (System.Exception ex)
+                    {
+                    	MessageBox.Show(ex.InnerException != null ? ex.InnerException.Message : ex.Message);
+                    }
+                }
+            }
+        }
+        /*
+        private List<string> ReadTextFileToList(string fileName)
+        {
+            List<string> readStreamReaderList = new List<string>();
+            using (FileStream iStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using(StreamReader reader = new StreamReader(iStream , Encoding.Default))
+                {
+                    reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    while (reader.Peek() > -1)
+                    {
+                        readStreamReaderList.Add(reader.ReadLine());
+                    }
+                    reader.Close();
+                    iStream.Close();
+                }
+            }
+            return readStreamReaderList;
+        }
+        */
+        private void LoadPictureBoxImg(string FileName)
+        {
+            pictureBox1.Load(FileName);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;            
+        }
+
+        private void BtnControl_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            int i = CurrentImgNum;
+            if (button.Name == "BtnLeft")
+                i--;
+            else
+                i++;
+
+            if (i < 0)
+                i = ListLoadFileName.Count - 1;
+            else if (i > ListLoadFileName.Count - 1)
+                i = 0;
+            pictureBox1.Load(ListLoadFileName[i]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            BtnCount.Text = string.Format("({0}/{1})", i + 1, ListLoadFileName.Count);
+            CurrentImgNum = i;
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            int i = CurrentImgNum;
+            i++;
+            if (i > ListLoadFileName.Count - 1)
+                i = 0;
+            pictureBox1.Load(ListLoadFileName[i]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            BtnCount.Text = string.Format("({0}/{1})", i + 1, ListLoadFileName.Count);
+            CurrentImgNum = i;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int i = CurrentImgNum;
+            if (e.Button == MouseButtons.Left)
+                i--;
+            else
+                i++;
+
+            if (i < 0)
+                i = ListLoadFileName.Count - 1;
+            else if (i > ListLoadFileName.Count - 1)
+                i = 0;
+
+            pictureBox1.Load(ListLoadFileName[i]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            BtnCount.Text = string.Format("({0}/{1})", i + 1, ListLoadFileName.Count);
+            CurrentImgNum = i;
+        }
+
+        private void LoadBtn_Img_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog();
+            if (ListLoadFileName[CurrentImgNum].Length > 0)
+                LoadPictureBoxImg(ListLoadFileName[CurrentImgNum]);
+        }
+    }
+}
