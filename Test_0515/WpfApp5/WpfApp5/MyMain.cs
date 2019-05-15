1abca58a9a69c5bc33a0baa8aef9870477e02f4e
JsShin0314 <nicesun314@gmail.com>
2019-05-15 17:20:01 +0900

    Test02 WPF 학습

---
+++ Test_0515/WpfApp5/WpfApp5/MyMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace WpfApp5
+{
+    class MyMain : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            MyMain app = new MyMain();
+            //app.ShutdownMode = ShutdownMode.OnMainWindowClose;//Main Window를 닫을 때 Sub Window도 같이 닫기.
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //Main Window
+            Window mainWindow = new Window();
+            mainWindow.Title = "WPF Sample(Main)";
+            mainWindow.MouseDown += WinMouseDown;
+            mainWindow.Show();
+
+            //Sub Window
+            for(int i =0;i<2;i++)
+            {
+                Window win = new Window();
+                win.Title = "Extra Window No." + (i + 1);
+                win.ShowInTaskbar = false;
+                win.Owner = mainWindow;
+                win.Show();
+            }
+        }
+
+        void WinMouseDown(object sender, MouseEventArgs args)
+        {
+            Window win = new Window();
+            win.Title = "Modal DialogBox";
+            win.Width = 400;
+            win.Height = 200;
+
+            Button b = new Button();
+            b.Content = "Click Me!";
+            b.Click += Button_Click;
+
+            win.Content = b;
+            win.ShowDialog(); // modal 형태
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Click!", sender.ToString());
+        }
+    }
+}
